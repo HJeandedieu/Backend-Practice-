2d313377934a2d3ab464321033654555f16f4333
jd <hashimweyesujeandedie@gmail.com>
2026-02-23 21:28:56 +0200

    Added express js functionality

---
+++ test.js
@@ -0,0 +1,11 @@
+const fs = require("fs")
+
+// fs.writeFile("new.txt", "Done By RedBlue JD.",{encoding: "utf-8", flag: "a"}, (err)=>{
+//     if(err){
+//         console.error("Couldn't write to file", err.message);
+//     }
+
+//     console.log("Content appended successfully")
+// })
+
+fs.readFile("data.txt", "r", "utf-8",)
