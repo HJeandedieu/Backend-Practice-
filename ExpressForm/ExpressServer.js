033258de1dad85d1f5995fbf391f1c80d66c768f
jd <hashimweyesujeandedie@gmail.com>
2026-02-23 22:22:55 +0200

    Initialised ExpressForm

---
+++ ExpressForm/ExpressServer.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({extended: false}))
+
+app.get("/", (req,res)=>{
+    res.sendFile(__dirname + "/index.html");
+})
+
+app.post("/submit-student-data", (req,res)=>{   
+    res.send(req.body.firstName + " " + req.body.lastName + " received")
+    console.log(req.body.firstName + " " + req.body.lastName + " received");
+})
+
+app.listen(port, ()=>{
+    console.log("server is running on port " + port)
+})
